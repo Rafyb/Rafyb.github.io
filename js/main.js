385a84f25b0cda61ab35d33d598b5a750ff812fd
Raphael Bauvin <rbauvin@yahoo.fr>
2020-01-31 18:44:25 +0100

    mobile test

--- js/main.js
+++ js/main.js
@@ -1,79 +1,88 @@
-homeRender();
+if(screen.width < 816){
+  document.querySelector('#mainBody').innerHTML = `
+    <h1> Cette page n'est pas encore disponible sur smartphone </h1>
+  `;
 
-document.querySelector(".home").onclick = function(event){  
-    event.preventDefault();
-    document.querySelector(".home").id ='activeA';
-    document.querySelector(".experience").id ='';
-    document.querySelector(".portfolio").id ='';
-    document.querySelector("#mydivheader").style.backgroundColor = "#0768a0" ;
-    homeRender();
-} ;
+} else {
 
-document.querySelector(".experience").onclick = function(event){
-    event.preventDefault();
-    document.querySelector(".home").id ='';
-    document.querySelector(".experience").id ='activeE';
-    document.querySelector(".portfolio").id ='';
-    document.querySelector("#mydivheader").style.backgroundColor = "#04912e" ;
-    experiencesRender();
+  homeRender();
 
-} ;
+  document.querySelector(".home").onclick = function(event){  
+      event.preventDefault();
+      document.querySelector(".home").id ='activeA';
+      document.querySelector(".experience").id ='';
+      document.querySelector(".portfolio").id ='';
+      document.querySelector("#mydivheader").style.backgroundColor = "#0768a0" ;
+      homeRender();
+  } ;
 
-document.querySelector(".portfolio").onclick = function(event){
-    event.preventDefault();
-    document.querySelector(".home").id ='';
-    document.querySelector(".experience").id ='';
-    document.querySelector(".portfolio").id ='activeR';
-    document.querySelector("#mydivheader").style.backgroundColor = "#c76912" ;
-    realisationsRender();
+  document.querySelector(".experience").onclick = function(event){
+      event.preventDefault();
+      document.querySelector(".home").id ='';
+      document.querySelector(".experience").id ='activeE';
+      document.querySelector(".portfolio").id ='';
+      document.querySelector("#mydivheader").style.backgroundColor = "#04912e" ;
+      experiencesRender();
 
-} ;
+  } ;
 
-document.getElementById("mydiv").style.display ="none";
+  document.querySelector(".portfolio").onclick = function(event){
+      console.log(event);
+      event.preventDefault();
+      document.querySelector(".home").id ='';
+      document.querySelector(".experience").id ='';
+      document.querySelector(".portfolio").id ='activeR';
+      document.querySelector("#mydivheader").style.backgroundColor = "#c76912" ;
+      realisationsRender();
 
-document.querySelector('.youtube').onclick = function(){
-    document.getElementById("mydiv").style.display ="block";
-};
+  } ;
 
-dragElement(document.getElementById("mydiv"));
+  document.getElementById("mydiv").style.display ="none";
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+  document.querySelector('.youtube').onclick = function(){
+      document.getElementById("mydiv").style.display ="block";
+  };
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+  dragElement(document.getElementById("mydiv"));
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+  function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+      // if present, the header is where you move the DIV from:
+      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      elmnt.onmousedown = dragMouseDown;
+    }
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
   }
 }
